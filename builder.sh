55a7dbda9a12554b6a946a7a7b048a640e7b2a06
Amidos <amidos@amidos.ru>
2015-12-13 22:29:45 +1100

    Allow custom source dirs in the resulting app image

--- builder.sh
+++ builder.sh
@@ -1,32 +1,99 @@
 #!/bin/bash
 
 # Ensure that there is a proper import path in the app package declaration
-cd /go/app
-pkgName="$(go list -e -f '{{.ImportComment}}' 2>/dev/null || true)"
+# Return package full name
+function check_package {
+	cd /go/app
+	local pkg_name="$(go list -e -f {{.ImportComment}} 2>/dev/null || true)"
 
-if [ -z "$pkgName" ];
-then
-  echo "Error: Root package should have a canonical import path declared"
-  exit 992
-fi
+	if [ -z "$pkg_name" ];
+	then
+		echo "Error: Root package should have a canonical import path declared"
+		exit 992
+	fi
 
-# Construct Go package path
-pkgPath="/go/src/$pkgName"
+	echo $pkg_name
+}
 
-# Setup src directory tree in GOPATH
-echo "Make dir $pkgPath"
-mkdir -p "$(dirname "$pkgPath")"
+# Construct Go full package path and return it
+function make_package_path {
+	local pkg_name=$1
+	local pkg_path="/go/src/$pkg_name"
 
-# Copy app source into GOPATH
-echo "Copy app source for building"
-cp -R /go/app/ $pkgPath
+	# Setup src directory tree in GOPATH
+	mkdir -p "$(dirname "$pkg_path")"
 
-# Compile statically linked version of the package
-cd $pkgPath
-echo "Building app $pkgName"
-`CGO_ENABLED=${CGO_ENABLED:-0} go build -o /app/app.run -a --installsuffix cgo --ldflags="${LDFLAGS:--s}" $pkgName`
+	# Copy app source into GOPATH
+	cp -R /go/app/ $pkg_path
 
-# Finally, dockerize the package
-tagName=$1
-echo "Building docker image $tagName"
-docker build -t $tagName -f /app/Dockerfile /app
+	echo $pkg_path
+}
+
+function parse_arguments {
+	local args=("$@")
+
+	while [[ $# > 1 ]]
+	do
+	key="$1"
+
+	case $key in
+	    -n|--name)
+		echo "Image name: $2"
+	    IMAGE_NAME="$2"
+	    shift
+	    ;;
+	    -d|--dirs)
+		echo "Include dirs: $2"
+	    INCLUDE_DIRS=$2
+	    shift # past argument
+	    ;;
+	    *)
+	        # unknown option
+	    ;;
+	esac
+	shift # past argument or value
+	done
+}
+
+function copy_app_dirs {
+	local inc_dirs=$1
+	local split_dirs=($inc_dirs)
+
+	echo "Copy selected app directories:"
+	cd /go/app
+	for i in "${split_dirs[@]}"
+	do
+		echo "...$i"
+		mkdir -p /app/build/$i
+		cd $i && cp -R . /app/build/$i
+		cd ..
+	done
+}
+
+# Compile statically linked version of package
+function compile_app {
+	pkg_name=$1
+	pkg_path=$2
+
+	echo "Building app $pkg_path"
+	cd $pkg_path
+	`CGO_ENABLED=${CGO_ENABLED:-0} go build -o /app/build/app.run -a --installsuffix cgo --ldflags="${LDFLAGS:--s}" $pkg_name`
+}
+
+function dockerize_app {
+	local image_name=$1
+	echo "Building docker image $IMAGE_NAME"
+	docker build -t $image_name -f /app/Dockerfile /app
+}
+
+pkg_name=$(check_package)
+echo "Check package: $pkg_name"
+
+pkg_path=$(make_package_path $pkg_name)
+echo "Package path: $pkg_path"
+
+parse_arguments "$@"
+
+copy_app_dirs "$INCLUDE_DIRS"
+compile_app $pkg_name $pkg_path
+dockerize_app $IMAGE_NAME
